e7a7553f8b2ac8b721ecfe1441202a0927869f36
Craig <cbmaruziva@hotmail.co.uk>
2021-12-28 20:32:45 +0000

    button added

--- index.js
+++ index.js
@@ -2,12 +2,14 @@
         //create function that makes get request to kanyerest
             //get and proccess  response
 
+const h1 = document.querySelector('h1')
+const button = document.querySelector('button')
+
 
 async function getQuote() {
     let response = await fetch("https://api.kanye.rest/");
     const data = await response.json()
-    console.log(data.quote)
-    
-    
+    console.log(data.quote)    
 }
+
 getQuote()
