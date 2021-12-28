cef60c20d1f4662ec03c9fb24d8cb9b5c09bd1b6
Sheb95 <80277508+Sheb95@users.noreply.github.com>
2021-12-28 21:06:58 +0000

    added quote history

--- index.js
+++ index.js
@@ -2,15 +2,34 @@
         //create function that makes get request to kanyerest
             //get and proccess  response
 
-const h1 = document.querySelector('h1')
-const button = document.querySelector('button')
+const list = document.querySelector('#kanye-quote-history');
+const button = document.querySelector('button'); 
+const quote = document.querySelector("#kanye-quote");
+const listItems = [];
+
+function addToList(quoteToAdd){
+    const newQuote = document.createElement("li");
+    newQuote.innerText = quoteToAdd;
+    list.appendChild(newQuote);
+}
 
 
 async function getQuote() {
     let response = await fetch("https://api.kanye.rest/");
-    const data = await response.json()
-    console.log(data.quote)    
-    h1.innerText = data.quote   
+    const data = await response.json();
+    console.log(data.quote);
+    quote.innerText = data.quote;
+
+    if(listItems.includes(data.quote) === false){
+        listItems.push(data.quote);
+        addToList(data.quote);
+    }
 }
 
 button.addEventListener("click",getQuote)
+
+// to-do: append previous quotes to body when button is clicked
+    // query select main container 
+    // create unordered list element 
+    // append new element to dom 
+
